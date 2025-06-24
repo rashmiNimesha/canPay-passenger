0d5750e92490f6edd305c1bd55f6be12b66e8e37
Nethmini Rajapaksha <nethminirajapaksha2001@gmail.com>
2025-06-25 02:39:35 +0530

    CanPay_Passenger :Implement Edit Name feature screens with frontend validations

---
+++ app/src/main/java/com/example/canpay_passenger/NameUpdateSuccessActivity.java
@@ -0,0 +1,28 @@
+package com.example.canpay_passenger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NameUpdateSuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_update_success);
+        Button btnContinue = findViewById(R.id.btnContinue);
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NameUpdateSuccessActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+}
