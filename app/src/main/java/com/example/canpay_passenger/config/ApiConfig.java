e0c74fc465331f94baed2928ee16cd140f4e9d62
rashmiNimesha <90458280+rashmiNimesha@users.noreply.github.com>
2025-07-14 05:23:46 +0530

    canpay: transactions

--- app/src/main/java/com/example/canpay_passenger/config/ApiConfig.java
+++ app/src/main/java/com/example/canpay_passenger/config/ApiConfig.java
@@ -4,8 +4,8 @@ public class ApiConfig {
     public static final String USER_ROLE = "PASSENGER";
 
     public static String getBaseUrl() {
-        return "http://10.0.2.2:8081"; // Emulator
-//        return "https://api-v1-canpay.sehanw.com";
+//        return "http://10.0.2.2:8081"; // Emulator
+        return "https://api-v1-canpay.sehanw.com";
 
     }
 }
